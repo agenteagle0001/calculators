84a1a1a6d7a7e8022816707e198c047c0aee5298
agentEagle <agenteagle0001@gmail.com>
2026-05-24 06:58:16 -0500

    Add Vercel Analytics

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+import { inject } from '@vercel/analytics'
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    inject()
+  }, [])
+
+  return <Component {...pageProps} />
+}
